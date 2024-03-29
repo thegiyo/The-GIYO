d1e5d708e8d09539dddcfd9aaed66da99979713f
thegiyo <165402873+thegiyo@users.noreply.github.com>
2024-03-29 11:22:30 -0400

    Giyo

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = ""
+global.botnumber = "8293720791"
 global.confirmCode = ""
 
 global.owner = [
